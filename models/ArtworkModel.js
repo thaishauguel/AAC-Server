4df78b1e6b9adc2909199d8dc73bcf86e9246e00
Magali <fort_magali@hotmail.fr>
2021-03-25 14:14:04 +0100

    modified artwork model

--- models/ArtworkModel.js
+++ models/ArtworkModel.js
@@ -1,39 +1,39 @@
-const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const artworkSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-        required: true,
-    },
-    artwork: {
-        type: String,
-        required: true
-    },
-    bids: [[{
-        bidder: ObjectId(user),
-        bid: Number
-    }]],
-    saleStatus: {
-        isForSale: {
-            type: Boolean,
-            default: false
-        },
-        initialPrice: {
-            type: Number,
-        }
-    }
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  forSale: {
+    type: Boolean,
+    default: false,
+  },
 });
-//IF (!forSale && bids.length !== 0) ==> Owner == bids[0][0].bidder(.username after populate)
 
 const ArtworkModel = mongoose.model("artworks", artworkSchema);
 module.exports = ArtworkModel;
+
+//IF (!forSale && bids.length !== 0) ==> Owner == bids[0][0].bidder(.username after populate)
 
 //             vente 1                                 vente 2
 // bid:[[{user1, 30},{user2, 20},{user1, 10}], [{user3, 30},{user5, 20},{user3, 10}], []]
